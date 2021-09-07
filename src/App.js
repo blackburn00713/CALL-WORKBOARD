de64e9b8211b3ca348337246c8a7e84dda84d162
nakullondhe <nakulprivate27@gmail.com>
2021-09-07 21:28:13 +0530

    bb

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import MiniDrawer from './components/MiniDrawer';
-import DHeader from './components/theme';
+// import DHeader from './components/theme';
+import './App.css';
+import { Box, Container } from '@material-ui/core';
 
 function App() {
   return (
     <div className="App">
+      <div>
       <MiniDrawer/>
-      <DHeader />
+      </div>
+      {/* <DHeader /> */}
+      <Container style={{padding: 0}}>ss</Container>
     </div>
   );
 }
